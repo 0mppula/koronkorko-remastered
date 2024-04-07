1bcdfe23a667f8c0cb7d6af9e97ecfb9d412db61
Omar Kraidié <omppula_1@hotmail.com>
2024-04-07 13:47:28 +0300

    Update logo and theme toggler styling in Nav components

--- components/Nav/Logo.tsx
+++ components/Nav/Logo.tsx
@@ -11,13 +11,17 @@ const Logo = () => {
 
 	return (
 		<div className="ml-4 self-center">
-			<Link href="/" className="gap-[0.35rem] items-center justify-center flex">
+			<Link
+				href="/"
+				className="gap-1 items-center justify-center flex ring-offset-background focus-visible:ring-2 focus-visible:outline-none focus-visible:ring-ring rounded-md p-1"
+			>
 				<Image
 					src={resolvedTheme === 'dark' ? logo_dark : logo_light}
-					alt="logo.jpg"
-					className="h-[34px] w-[34px] cursor-pointer select-none"
+					alt="KoronKorko Logo"
+					className="h-[30px] w-[30px] cursor-pointer select-none min-h-[30px] min-w-[30px]"
 				/>
-				<p className="font-bold text-2xl">KoronKorko</p>
+
+				<p className="font-bold text-2xl hidden sm:block">KoronKorko</p>
 			</Link>
 		</div>
 	);
