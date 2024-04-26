32fcd6220487c9c69d6f897d580a0c18b0dd0fdb
Omar Kraidié <omppula_1@hotmail.com>
2024-04-26 12:25:43 +0300

    Fixed report text overflow.

--- components/Report/ReportGroup.tsx
+++ components/Report/ReportGroup.tsx
@@ -10,13 +10,15 @@ const ReportGroup = ({ header, value, fullWidth = false }: ReportGroupProps) => 
 	return (
 		<div
 			className={cn(
-				'grow shrink basis-full sm:basis-[calc(50%-1rem/2)]',
+				'grow shrink basis-full sm:basis-[calc(50%-1rem/2)] justify-start',
 				fullWidth && 'sm:basis-full'
 			)}
 		>
 			<h3 className="text-xl font-normal mb-1.5">{header}</h3>
 
-			<h4 className="scroll-m-20 text-3xl font-bold transition-colors first:mt-0">{value}</h4>
+			<h4 className="scroll-m-20 text-3xl font-bold transition-colors first:mt-0 sm:max-w-[672px] break-all sm:break-keep">
+				{value}
+			</h4>
 		</div>
 	);
 };
